a14296acd970891c2d15fbd4c9693b3fd5d10252
Tim Würtele <tim.wuertele@sec.uni-stuttgart.de>
2021-10-26 14:41:05 +0200

    serve.sh: Add output so user knows what's happening

--- serve.sh
+++ serve.sh
@@ -1,5 +1,7 @@
 #!/bin/bash -e
 
+echo "### Starting local web server (localhost:4000), this might take a moment..."
+
 source env.sh
 
 docker run --rm   -v "${PWD}:/srv/jekyll" -p 4000:4000 jekyll/jekyll:${JEKYLL_VERSION} jekyll serve
